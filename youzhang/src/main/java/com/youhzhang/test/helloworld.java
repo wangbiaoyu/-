078a720f8c84e511be9afc068b447963e63ba4e1
youzhang <952927759@qq.com>
2019-12-14 22:37:39 +0800

    test

---
+++ youzhang/src/main/java/com/youhzhang/test/helloworld.java
@@ -0,0 +1,12 @@
+package com.youhzhang.test;
+
+/**
+ * @ClassName: helloworld
+ * @author: youzhang
+ * @date: 2019/12/14  22:36
+ */
+public class helloworld {
+    public static void main(String[] args) {
+        System.out.println("helloworld");
+    }
+}
