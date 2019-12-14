fa0be66b2a60d7deedb55ed361b4dd669ec54b55
youzhang <952927759@qq.com>
2019-12-14 22:45:02 +0800

    test

--- youzhang/src/main/java/com/youhzhang/test/helloworld.java
+++ youzhang/src/main/java/com/youhzhang/test/helloworld.java
@@ -7,6 +7,6 @@ package com.youhzhang.test;
  */
 public class helloworld {
     public static void main(String[] args) {
-        System.out.println("helloworld");
+        System.out.println("helloworld"); 
     }
 }
