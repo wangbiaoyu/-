009cac5a769495b1db2de604e1153d6f38d0f341
wangbiaoyu <1017678871@qq.com>
2019-12-17 00:04:15 +0800

    32

--- 1.汉明距离/位运算.cpp
+++ 1.汉明距离/位运算.cpp
@@ -11,3 +11,34 @@ public:
 		return res;
 	}
 };
+
+
+
+class Solution {
+public:
+	int hammingDistance(int x, int y) {
+		int n1[32] = { 0 };
+		int n2[32] = { 0 };
+		int x1 = abs(x);
+		int x2 = abs(y);
+		int ind = 0;
+		while (x1 != 1 && x1 != 0){
+			n1[ind++] = x1 % 2;
+			x1 /= 2;
+		}
+		n1[ind] = x1;
+		ind = 0;
+		while (x2 != 1 && x2 != 0){
+			n2[ind++] = x2 % 2;
+			x2 /= 2;
+		}
+		n2[ind] = x2;
+		if (x < 0) n1[31] = 1;
+		if (y < 0) n2[31] = 1;
+		int res = 0;
+		for (int i = 0; i < 32; i++){
+			res += ((n1[i] == n2[i]) ? 0 : 1);
+		}
+		return res;
+	}
+};
